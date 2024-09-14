1c955b98971aea86f6fd8f3faf3078519a142524
BenMoreira <bmoreira@ggc.edu>
2024-09-14 15:57:53 -0400

    Small changes

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
             </button>
           </div>
         </div> :
-        <div className='w-full min-h-screen bg-[url(./assets/bg.webp)] bg-cover bg-center'>
+        <div className='w-full bg-[url(./assets/bg.webp)] bg-cover bg-center'>
           <motion.div
             initial={{
               opacity: 0
@@ -37,54 +37,54 @@ function App() {
               }
             }}
           >
-          <div className='w-full h-screen flex flex-col items-center gap-5'>
+          <div className='w-full h-screen flex flex-col items-center gap-3'>
             <div className='w-full flex flex-col items-start'>
               <img src={bluey} className='w-[150px] h-[180px]' />
             </div>
 
             {spanish ? 
-              <div className='font-bold text-3xl'>¡Estás invitado a la Primera</div> :
-              <div className='font-bold text-3xl'>You're invited to Analeia's</div>
+              <div className='font-bold text-2xl'>¡Estás invitado a la Primera</div> :
+              <div className='font-bold text-2xl'>You're invited to Analeia's</div>
             }
 
             {spanish ? 
-              <div className='font-bold text-3xl'>Fiesta de</div> :
+              <div className='font-bold text-2xl'>Fiesta de</div> :
               <div className='hidden'></div>
             }
 
             {spanish ? 
-              <div className='font-bold text-3xl'>Cumpleaños de Analeia!</div> :
-              <div className='font-bold text-3xl'>1st Birthday Party!</div>
+              <div className='font-bold text-2xl'>Cumpleaños de Analeia!</div> :
+              <div className='font-bold text-2xl'>1st Birthday Party!</div>
             }
 
             {spanish ?
-              <div className='text-2xl'>Ven con nosotros a celebrar</div> :
-              <div className='text-2xl'>Join us to celebrate</div>
+              <div className='text-xl'>Ven con nosotros a celebrar</div> :
+              <div className='text-xl'>Join us to celebrate</div>
             }
 
             {spanish ? 
-              <div className='font-bold text-3xl'>Analeia Jade Torres</div> :
-              <div className='font-bold text-3xl'>Analeia Jade Torres</div>
+              <div className='font-bold text-2xl'>Analeia Jade Torres</div> :
+              <div className='font-bold text-2xl'>Analeia Jade Torres</div>
             }
 
             {spanish ?
-              <div className='text-2xl'>Sábado, Octubre 5 a las 0:00 PM</div> :
-              <div className='text-2xl'>Saturday, October 5th at 0:00 PM</div>
+              <div className='text-xl'>Sábado, Octubre 5 a las 0:00 PM</div> :
+              <div className='text-xl'>Saturday, October 5th at 0:00 PM</div>
             }
 
             {spanish ?
-              <div className='text-2xl'>3896 Egypt Rd., Snellville, Georgia, 30039</div> :
-              <div className='text-2xl'>3896 Egypt Rd., Snellville, Georgia, 30039</div>
+              <div className='text-xl'>3896 Egypt Rd., Snellville, Georgia, 30039</div> :
+              <div className='text-xl'>3896 Egypt Rd., Snellville, Georgia, 30039</div>
             }
 
             {spanish ?
-              <div className='text-2xl'>¿No sabes qué regalarle?</div> :
-              <div className='text-2xl'>Don't know what to get her?</div>
+              <div className='text-xl'>¿No sabes qué regalarle?</div> :
+              <div className='text-xl'>Don't know what to get her?</div>
             }
 
             {spanish ?
-              <div className='text-2xl'>Mira aquí: <a className='cursor font-bold underline text-blue-600' href='https://www.amazon.com/hz/wishlist/ls/H3T2MBHXXZHC?ref_=wl_share'>Amazon</a></div> :
-              <div className='text-2xl'>Check here: <a className='cursor font-bold underline text-blue-600' href='https://www.amazon.com/hz/wishlist/ls/H3T2MBHXXZHC?ref_=wl_share'>Amazon</a></div>
+              <div className='text-xl'>Mira aquí: <a className='cursor font-bold underline text-blue-600' href='https://www.amazon.com/hz/wishlist/ls/H3T2MBHXXZHC?ref_=wl_share'>Amazon</a></div> :
+              <div className='text-xl'>Check here: <a className='cursor font-bold underline text-blue-600' href='https://www.amazon.com/hz/wishlist/ls/H3T2MBHXXZHC?ref_=wl_share'>Amazon</a></div>
             }
 
             <div className='w-full flex flex-col items-end'>
